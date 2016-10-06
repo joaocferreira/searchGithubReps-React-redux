8f51d7def11c5b3658d310e867513d9a91e22d1d
joaocferreira <joaocferreira>
2016-10-06 12:18:46 +0200

    remove old props

--- app/src/index.jsx
+++ app/src/index.jsx
@@ -9,18 +9,12 @@ import NotFound from './components/error/404.jsx';
 import { Provider } from 'react-redux';
 import store from './store';
 
-const historyArray = [];
-
-const addHistory = (query) => {
-	historyArray.push(query);
-};
-
 ReactDOM.render((
 	<Provider store={store}>
 		<Router history={browserHistory}>
 			<Route path='/' component={Main}>
-    		<IndexRoute component={SearchContainer} addHistory={addHistory} />
-    		<Route path='/history' component={History} historyArray={historyArray} />
+    		<IndexRoute component={SearchContainer} />
+    		<Route path='/history' component={History} />
     		<Route path='/about' component={About} />
     		<Route path='*' component={NotFound} />
     	</Route>
